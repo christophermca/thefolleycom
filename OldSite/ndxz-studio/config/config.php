954adadcfdeb8b69aba66a5272035dfe0d55cec1
Christopher MCAdams <cmcadams@cars.com>
2015-03-28 15:54:03 -0500

    removed files

--- OldSite/ndxz-studio/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php  if (!defined('SITE')) exit('No direct script access allowed');
-
-$indx['db'] 		= 'thefoll1_folley';
-$indx['user'] 		= 'thefoll1_folley';
-$indx['pass'] 		= 'mocha11787';
-$indx['host'] 		= 'localhost';
-$indx['sql']		= 'mysql';
-
-?>
